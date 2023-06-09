c1d7e013784b11c09e63710a3c73c977a5aeff26
MohamadMahdi RaeisiMakiani <66519118+MohamadMahdi-RaeisiMakiani@users.noreply.github.com>
2023-06-09 14:56:24 -0500

    Add files via upload

---
+++ assets/scripts/scripts_MainPage.js
@@ -0,0 +1,6 @@
+$(document).ready(function main() {
+	let main_header_content = $(".mainPage-header").text();
+	console.log(`This is the message > ${main_header_content}`);
+	console.log(main_header_content);
+	console.log(document.querySelector(".mainPage-header"));
+});
